168a53bfbbf2b023b6e3730558b505094e5b83b0
akakuznecov5644-sudo <akakuznecov5644@gmail.com>
2026-02-19 11:25:08 +0100

    Add Login back to Open Jackpot Create Form

---
+++ libs/Jackpots.py
@@ -0,0 +1,9 @@
+"""Robot Framework library for Jackpot-related keywords."""
+
+
+class Jackpots:
+    """Placeholder library for Jackpot keywords."""
+
+    def ping_jackpots(self):
+        """Simple keyword to verify library import."""
+        return "Jackpots library is loaded"
